ef54a437ca3770d21de37f625db06bb44ed85950
Sonal Ranjit <sonal.ranjit3@gmail.com>
2020-04-05 15:10:41 -0400

    finished chapter 5

---
+++ ch05-using-structs-to-structure-related-data/listing-05-05/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+}
+
