cd5bcfb8b0b4d77bc6fbce0ba43bd40a235111b5
Sonal Ranjit <sonal.ranjit3@gmail.com>
2020-09-12 13:09:18 -0400

    finished chapter 16

---
+++ ch16-fearless-concurrency/listing-16-04/src/main.rs
@@ -0,0 +1,14 @@
+use std::thread;
+
+fn main() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(|| {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    drop(v);
+
+    handle.join().unwrap();
+}
+
